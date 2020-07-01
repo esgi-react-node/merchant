038003542e3d502807daa4e02e52868446d2fae9
Quentin Hermiteau <quentinhermiteau@gmail.com>
2020-07-01 15:11:39 +0200

    add router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 import Order from "./components/Order";
 import Transaction from "./components/Transaction";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router";
 
 function App() {
-
   return (
     <div className="App">
-      <Order></Order>
-      <Transaction></Transaction>
+      <nav>
+        <li><a href="/order">Order</a></li>
+        <li><a href="/transactions">Transactions</a></li>
+      </nav>
+      <Router>
+        <Switch>
+          <Route path="/order" component={Order} />
+          <Route path="/transactions" component={Transaction} />
+        </Switch>
+      </Router>
     </div>
   );
 }
