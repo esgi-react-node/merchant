35103a7a0d28e156cf862190d00ccd2c630c1dd3
lucas <xz351@hotmail.fr>
2020-07-01 22:57:16 +0200

    Update server config

--- server/server.js
+++ server/server.js
@@ -4,12 +4,7 @@ const RouterManager = require("./routes");
 const app = express();
 
 app.use(express.json());
-
-app.get("/hello", (req, res, next) => {
-  console.log(req.query);
-  res.json({ msg: "Hello" });
-});
-
+app.use(express.urlencoded());
 RouterManager(app);
 
 app.listen(3000, () => console.log("listening..."));
